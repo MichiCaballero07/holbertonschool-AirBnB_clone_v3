6f03fcbbeb904ee0a6cf45644ae3c41785353fb5
MichiCaballero07 <125148658+MichiCaballero07@users.noreply.github.com>
2023-08-23 16:08:34 -0500

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -25,6 +25,7 @@ class DBStorage:
     __engine = None
     __session = None
 
+'''
     def __init__(self):
         """Instantiate a DBStorage object"""
         HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
@@ -89,3 +90,4 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+'''
