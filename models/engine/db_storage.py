39b98ca3b8313ad5d3c9f67c9bfc1377672cd08c
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-08-25 12:18:24 -0500

    update files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -51,23 +51,8 @@ class DBStorage:
                     new_dict[key] = obj
         return (new_dict)
 
-    def get(self, cls, id):
-        """get an object based on its class and id"""
-
-        my_dict = self.all(cls)
-        for obj in my_dict.values():
-            if obj.id == id:
-                return obj
-        return None
-
-    def count(self, cls=None):
-        """counts objects in the database"""
-
-        my_dict = self.all(cls)
-        return (len(my_dict))
-
     def new(self, obj):
-        """method to add new obj to database"""
+        """add the object to the current database session"""
         self.__session.add(obj)
 
     def save(self):
@@ -89,3 +74,19 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """ retrieves """
+        if cls in classes.values() and id and type(id) == str:
+            d_obj = self.all(cls)
+            for key, value in d_obj.items():
+                if key.split(".")[1] == id:
+                    return value
+        return None
+
+    def count(self, cls=None):
+        """ counts """
+        data = self.all(cls)
+        if cls in classes.values():
+            data = self.all(cls)
+        return len(data)
